4ccda54e29e3aaff6c04e1b973a3f519d6406f80
jemikanegara <jemikanegara@gmail.com>
2018-11-22 18:31:50 +0700

    add google map in addgig component

--- src/components/Add/index.js
+++ src/components/Add/index.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
 import "./index.css";
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 
 class Add extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { address: "", latLng: "" };
   }
+
+  handleChange = address => {
+    this.setState({ address });
+  };
+
+  handleSelect = address => {
+    geocodeByAddress(address)
+      .then(results => getLatLng(results[0]))
+      .then(latLng => {
+        this.setState({
+          latLng: `${latLng.lat}, ${latLng.lng}`
+        });
+        console.log("Success", latLng.lat);
+      })
+      .catch(error => console.error("Error", error));
+  };
   render() {
     return (
       <div className="add-container">
@@ -47,7 +67,72 @@ class Add extends Component {
         />
 
         <div className="add-gig-location">
-          <input type="text" placeholder="Location" />
+          <PlacesAutocomplete
+            value={this.state.address}
+            onChange={this.handleChange}
+            onSelect={this.handleSelect}
+          >
+            {({
+              getInputProps,
+              suggestions,
+              getSuggestionItemProps,
+              loading
+            }) => (
+              <div>
+                <input
+                  {...getInputProps({
+                    placeholder: "Search Places ...",
+                    className: "location-search-input"
+                  })}
+                />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? "suggestion-item--active"
+                      : "suggestion-item";
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: "#fafafa", cursor: "pointer" }
+                      : { backgroundColor: "#ffffff", cursor: "pointer" };
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+        </div>
+
+        <div>
+          {this.state.latLng !== "" && (
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${
+                this.state.latLng
+              }`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="add-gig-locview"
+            >
+              <img
+                className="add-gig-map"
+                src={`https://maps.googleapis.com/maps/api/staticmap?center=${
+                  this.state.latLng
+                }&zoom=14&size=350x250&markers=color:red%7Clabel:L%7C${
+                  this.state.latLng
+                }&key=AIzaSyClsojZYYr5d3CQRJpfwDOWozk1bZ3190w`}
+                alt="gig-address"
+              />
+            </a>
+          )}
         </div>
         <div className="add-gig-submit">
           <input type="submit" value="Submit" />
