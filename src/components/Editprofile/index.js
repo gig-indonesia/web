4364ed32df35a2071d9756cacfd5699c09769683
adam fadly <adamfadly11@gmail.com>
2018-11-29 12:27:57 +0700

    edit profile

--- src/components/Editprofile/index.js
+++ src/components/Editprofile/index.js
@@ -4,10 +4,25 @@ import "./index.css";
 class EditProfile extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      image: null,
+      about: "",
+      skills: "",
+      contact: "",
+      email: ""
+    };
   }
   handleImageChange = e => {
     this.setState({ image: e.target.files[0] });
+  };
+
+  handleOnChange = e => {
+    console.log(e);
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    const data = await new FormData();
   };
 
   render() {
@@ -21,7 +36,7 @@ class EditProfile extends Component {
             })`
           }}
         >
-          <label htmFor="edit-image-photo">
+          <label htmlFor="edit-image-photo">
             {!this.state.image && (
               <div>
                 <i className="fas fa-plus" />
@@ -32,7 +47,7 @@ class EditProfile extends Component {
         </div>
 
         <input
-          id="add-gig-image"
+          id="edit-image-photo"
           accept="image/*"
           type="file"
           placeholder="Add photo"
@@ -43,22 +58,37 @@ class EditProfile extends Component {
 
         <div className="edit-about">
           <p>About</p>
-          <input />
+          <input
+            placeholder="About"
+            type="text"
+            name="about"
+            onChange={this.handleOnChange}
+            value={this.state.about}
+          />
 
-          <p>skills</p>
-          <input className="edit-text-area" />
+          <p>Skills</p>
+          <input
+            className="edit-text-area"
+            placeholder="Skills"
+            type="text"
+            name="skills"
+            onChange={this.handleOnChange}
+            value={this.state.skills}
+          />
 
           <p>contact</p>
           <input
             className="edit-text-area"
             name="Phone"
             placeholder="Phone number"
+            type="text"
+            onChange={this.handleOnChange}
+            value={this.state.contact}
           />
-          <input
-            className="edit-text-area"
-            name="email"
-            placeholder="email or username"
-          />
+
+          <button type="submit" value="Submit" onClick={this.handleSubmit}>
+            submit
+          </button>
         </div>
       </div>
     );
