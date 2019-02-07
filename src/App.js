419587724e05b1db2f500ce4aa9d4175255f6865
jemikanegara <jemikanegara@gmail.com>
2019-02-07 15:30:42 +0800

    add basename to react router to get indexed by google

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div className="background">
           <div className="container">
             <Header />
